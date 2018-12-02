152119dd1049301db638d904a3609a7d862b741f
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-12-02 12:02:16 -0800

    solves 2

--- code-challenges/301/includes-every-substring-charat/challenges-13.test.js
+++ code-challenges/301/includes-every-substring-charat/challenges-13.test.js
@@ -25,7 +25,13 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 ------------------------------------------------------------------------------------------------ */
 
 const findHappiness = (arr) => {
-  // Solution code here...
+  let resultArr = [];
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i].includes(':)')){
+      resultArr.push(arr[i]);
+    };
+  }
+  return resultArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
