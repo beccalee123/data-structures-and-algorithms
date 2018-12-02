1f687180e2db8192c6c629a8431619ec694ae2b8
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-12-01 18:26:22 -0800

    a couple of attempts

--- code-challenges/301/chaining-methods/challenges-10.test.js
+++ code-challenges/301/chaining-methods/challenges-10.test.js
@@ -12,11 +12,14 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // for(let i = 0; i < input.length; i++){
-    //iterate through array of arrays
-    //use arr.reduce to accumulate instances of target number
-    //return accumulator
-  };
+  return input.reduce((accumulator, value) => {
+      for(let i = 0; i < input.length; i++){
+        if (value === target){
+          accumulator++
+        }
+        return accumulator;
+    }0});
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -29,12 +32,11 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  //let newSum = something
-  // for(let i = 0; i < input.length; i++){
-    //iterate through array of arrays
-    //use arr.reduce and accumulator to add values
-    //}
-  //return newSum
+  let newSum = 0
+  for(let i = 0; i < input.length; i++){
+    newSum + input[i]
+  }
+  return newSum
 };
 
 /* ------------------------------------------------------------------------------------------------
