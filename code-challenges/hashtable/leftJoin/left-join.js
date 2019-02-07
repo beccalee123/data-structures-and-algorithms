581f018a2318ff209550a6fdc4ddbfe8ac850f23
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-02-06 20:06:50 -0800

    Adds testing

--- code-challenges/hashtable/leftJoin/left-join.js
+++ code-challenges/hashtable/leftJoin/left-join.js
@@ -9,7 +9,6 @@ function leftJoin(left, right){
     if(left.table[i]){
       let leftv = Object.keys(left.table[i].head.value)[0];
       if(right.contains(leftv)){
-        // let ri = right.getHash(leftv);
         current.push(leftv);
         current.push(left.table[i].head.value[leftv]); 
         current.push(right.find(leftv)[leftv]);
@@ -18,18 +17,20 @@ function leftJoin(left, right){
       result.push(current);
     }
   }
-  console.log('result', result);
+  // console.log('result', result);
   return result;
 }
 
-let myHash1 = new Hashtable(50);
-myHash1.add('Happy', 'Elated');
-myHash1.add('Fond', 'Enamored');
-myHash1.add('Sad', 'Melancholy');
+// let myHash1 = new Hashtable(50);
+// myHash1.add('Happy', 'Elated');
+// myHash1.add('Fond', 'Enamored');
+// myHash1.add('Sad', 'Melancholy');
 
-let myHash2 = new Hashtable(50);
-myHash2.add('Happy', 'Sad');
-myHash2.add('Mad', 'Not Mad');
-myHash2.add('Fond', 'Averse');
+// let myHash2 = new Hashtable(50);
+// myHash2.add('Happy', 'Sad');
+// myHash2.add('Mad', 'Not Mad');
+// myHash2.add('Fond', 'Averse');
 
-leftJoin(myHash1, myHash2);
+// leftJoin(myHash1, myHash2);
+
+module.exports = leftJoin;
